96b1b91b132e1dffa7b761be5d7d245a7eff50f0
Jack Collins <Blueskyflyer2@gmail.com>
2013-01-06 12:09:03 -0600

    set initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module BlueskybreweryApp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Trying to get an error off heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
