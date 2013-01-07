5b68bf46401e86dfbebe6720128f50bf53531fd7
Jack Collins <Blueskyflyer2@gmail.com>
2013-01-06 19:09:46 -0600

    changed style migration

--- db/migrate/20121217042051_create_styles.rb
+++ db/migrate/20121217042051_create_styles.rb
@@ -1,18 +1,6 @@
 class CreateStyles < ActiveRecord::Migration
   def change
     create_table :styles do |t|
-  	end
-  	Style.create :name => "American IPA"
-  	Style.create :name => "Imperial IPA"
-  	Style.create :name => "IPA"
-  	Style.create :name => "Hefeweizen"
-  	Style.create :name => "Weizenbock"
-  	Style.create :name => "Witbier"
-  	Style.create :name => "Pilsner"
-  	Style.create :name => "Pale Ale"
-  	Style.create :name => "Porter"
-  	Style.create :name => "Stout"
-  end
-      t.timestamps
+    t.timestamps
   end
 end
