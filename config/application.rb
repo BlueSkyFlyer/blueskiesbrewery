8965dccf23aeedffd98fa6c558ea0a65deb4e8e0
Dave Collins <davecollins@cc-logic.com>
2013-03-31 16:47:04 -0500

    putting initialize_on_precompile back to false

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module BlueskybreweryApp
     config.assets.enabled = true
 
     # Trying to get an error off heroku
-   # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
