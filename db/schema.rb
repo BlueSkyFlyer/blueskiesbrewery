e1b20e37238e419e7025f43243c129fb36d94318
Jack Collins <Blueskyflyer2@gmail.com>
2013-05-13 20:44:12 -0500

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407151630) do
+ActiveRecord::Schema.define(:version => 20130514014219) do
 
   create_table "beers", :force => true do |t|
     t.datetime "created_at",         :null => false
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(:version => 20130407151630) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
